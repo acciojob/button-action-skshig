fc3dd838fd9675175faf9634f1d3c13ed794772a
skshig <skshig@gmail.com>
2023-07-19 13:50:45 +0530

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+let [visibile, setVisible] = useState(false)
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id="click" onClick={()=> setVisible(true)}>
+        Click
+      </button>
+      {
+        visibile && 
+        <p id="para">
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+        </p>
+      }
     </div>
   );
 }
